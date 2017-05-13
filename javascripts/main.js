6795a674cb25970585a0851f9f5ca71c760f64d5
sharon smith <sharon.e.smith2@gmail.com>
2017-05-12 20:34:50 -0500

    completed zoeFood functionality

--- javascripts/main.js
+++ javascripts/main.js
@@ -3,13 +3,20 @@ var app = angular.module("zoeFoods", []);
 
 app.controller("FoodCtrl", ($scope) => {
 
-$scope.tryThis = () => {
+	$scope.buttonLabel = "Click for Recipe!";
+
+	$scope.tryThis = () => {
 		$scope.showPie = true;
 	};
 
 	$scope.recipe = () => {
 		$scope.showPie = false;
 		$scope.showRecipe = true;
+		$scope.buttonLabel = "Clear";
+	};
+
+	$scope.clearAll = () => {
+		$scope.showRecipe = false;
 	};
 
 });
